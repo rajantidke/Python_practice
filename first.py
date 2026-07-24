b0b8f6037d74b4fa8485f93b58ac311ef5a7dba7
rtidke3 <your-gatech-email@gatech.edu>
2026-07-23 22:23:41 -0400

    First commit, some progress on jupyter

---
+++ first.py
@@ -0,0 +1,24 @@
+# print("Hello Wordl")
+# print("World Hello")
+
+# message = "Hello World!"
+# print(message)
+
+# multiline_text = """ Rajan was learning python 
+# , and we'll see how lng he can continue
+# without giving up"""
+# print(multiline_text)
+
+
+# language = "Python"
+
+# if language == "Python":
+#     print("Codititional is true")
+
+#################################################################
+#################### TUTORIAL 2 - STRINGS #################################
+##################################################################
+
+
+message = "Hello World"
+print(message)
